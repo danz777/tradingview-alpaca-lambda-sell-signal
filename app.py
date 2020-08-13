d3296eef3ced31d495780d291d0183c2a87010b0
danz777 <40545998+danz777@users.noreply.github.com>
2020-08-13 06:38:39 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import requests, json
+from chalice import Chalice
+
+app = Chalice(app_name='sell')
+
+API_KEY = 'PK81FNC68XKM2JLETAFQ'
+SECRET_KEY = 'GGKwn0HPlhMOz88OXLlLgnYZPXnzDxBmVp31CWvb'
+BASE_URL = "https://paper-api.alpaca.markets"
+ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+
+@app.route('/sell', methods=['POST'])
+def create_order(symbol, qty, side, type, time_in_force):
+    request = app.current_request
+    webhook_message = request.json_body
+
+    data = {
+        "symbol": webhook_message['ticker'],
+        "qty": '10',
+        "side": 'sell',
+        "type": 'market',
+        "time_in_force": 'gtc'
+    }
+    r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+    response = json.loads(r.content)
+        
+    return {
+        'webhook_message': webhook_message,
+        'id': response['id'],
+        'client_order_id': response['client_order_id']
+    }
